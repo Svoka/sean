dbe8f96e0ef7d18f1a51f74a3ed7b087599209f5
Artem Osipov <aosipov@swooapp.com>
2024-09-19 19:58:18 +0300

    Update kotlin and ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
